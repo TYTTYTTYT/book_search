dd3db69328b922feed7b184d199b2c05751a5c73
TYTTYTTYT <tai.yintao@gmail.com>
2023-03-04 20:20:02 +0000

    update start code

--- start_server.sh
+++ start_server.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 cd /home/tai_yintao101/book_search
-screen -S index -dm index_server/run.sh
-screen -S colbert -dm colbert_code/run.sh
-screen -S db -dm database_code/run.sh
-screen -S cache -dm message_server/run.sh
+screen -S index -dm bash index_server/run.sh
+screen -S colbert -dm bash colbert_code/run.sh
+screen -S db -dm bash database_code/run.sh
+screen -S cache -dm bash message_server/run.sh
