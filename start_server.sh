a9df641de7a5e8220b4ace257fe3a125391d0387
TYTTYTTYT <tai.yintao@gmail.com>
2023-03-05 00:24:22 +0000

    use compiled jar

--- start_server.sh
+++ start_server.sh
@@ -3,5 +3,5 @@ cd /home/tai_yintao101/book_search
 screen -S index -dm bash index_server/run.sh
 screen -S colbert -dm bash colbert_code/run.sh
 screen -S db -dm bash database_code/run.sh
-sleep 120
-screen -S cache -dm bash message_server/run.sh
+sleep 30
+screen -S cache -dm java -jar messageserver-0.0.1-SNAPSHOT.jar
