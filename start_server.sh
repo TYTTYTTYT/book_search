ba69bb1bd61f85a1d548174ac53ae2ae8ae7a4ec
TYTTYTTYT <tai.yintao@gmail.com>
2023-03-05 00:00:24 +0000

    add sleep time before message server start

--- start_server.sh
+++ start_server.sh
@@ -3,4 +3,5 @@ cd /home/tai_yintao101/book_search
 screen -S index -dm bash index_server/run.sh
 screen -S colbert -dm bash colbert_code/run.sh
 screen -S db -dm bash database_code/run.sh
+sleep 120
 screen -S cache -dm bash message_server/run.sh
