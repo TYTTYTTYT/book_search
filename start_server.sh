d8854bbd832001923b1499e7f33fb0cdcac08e66
TYTTYTTYT <tai.yintao@gmail.com>
2023-03-06 22:17:45 +0000

    tested start script

--- start_server.sh
+++ start_server.sh
@@ -3,5 +3,3 @@ cd /home/tai_yintao101/book_search
 screen -S index -dm bash index_server/run.sh
 screen -S colbert -dm bash colbert_code/run.sh
 screen -S db -dm bash database_code/run.sh
-sleep 30
-screen -S cache -dm java -jar messageserver-0.0.1-SNAPSHOT.jar
