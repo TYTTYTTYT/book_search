af3a8c56fd5280f16f2d42975438999dd8b62bd8
adqaicp <adqaicp@gmail.com>
2023-03-09 17:42:07 +0000

    live index py

--- colbert_code/index_update.py
+++ colbert_code/index_update.py
@@ -7,14 +7,17 @@ import csv
 import os
 import sys
 
+# ColBERT root path
+sys.path.insert(0, '/home/adqaicp/documents/ColBERT')
+
+# ColBERT imports
 from colbert.infra import Run, RunConfig, ColBERTConfig
 from colbert.data import Queries, Collection
 from colbert import Indexer, Searcher
 
-
-checkpointp = 'ColBERT/docs/downloads/colbertv2.0'
+# Define the path before running the code
+checkpointp = '/home/adqaicp/documents/ColBERT/docs/downloads/colbertv2.0'
 index_name = 'colbertv2.0'
-collection = 'collection.tsv'
 
 
 def getvalue(argv):
@@ -22,20 +25,20 @@ def getvalue(argv):
     try:
         opts, args = getopt.getopt(argv,"hi:o:",["ifile=","ofile="])
     except getopt.GetoptError:
-        print('test.py -i <inputfile> -o <outputfile>\n')
+        print('-i <inputfile> -o <index-output-path>\n or -h for help')
         sys.exit(2)
     for opt, arg in opts:
         if opt == '-h':
-            print('test.py -i <inputfile> -o <outputfile>')
-            print('inputfile: json file')
-            print('outputfile: tsv file')
+            print('\n------ help ------\n-i <inputfile> -o <index-output-path>\n')
+            print('<inputfile>: \njson file ->> example: test.json\n')
+            print('<index-output-path>: \nthe path to store the index ->> example: /home/user/ColBERT')
             sys.exit()
         elif opt in ("-i", "--ifile"):
             inputfile = arg
         elif opt in ("-o", "--ofile"):
-            outputfile = arg
+            outputpath = arg
 
-    return inputfile, outputfile
+    return inputfile, outputpath
 
 def jsontotsv(inputfile):
     with open(inputfile, "r", encoding="utf-8") as f:
@@ -60,13 +63,20 @@ def jsontotsv(inputfile):
         passage_id.append(i["book_id"])
         n = n + 1
 
-    with open('collection.tsv', 'w', encoding="utf-8") as f:
+    # replace the inputfile name with outputfile name
+    outputtsv = inputfile.replace(".json", ".tsv")
+
+    with open(outputtsv, 'w', encoding="utf-8") as f:
         tsv_w = csv.writer(f, delimiter='\t', lineterminator='\n')
         # tsv_w.writerow(['label', 'question'])
         for num in range(len(lines)):
             tsv_w.writerow([label[num], question[num]])
+    
+    return outputtsv
 
-def indexer(checkpointp, index_name, collection):
+def indexer(checkpointp, index_name, collection, outputpath):
+    experiment = 'liveindex'
+    index_folder = 'experiments/' + experiment + '/indexes/'
     nbits = 2   # encode each dimension with 2 bits
     doc_maxlen = 300   # truncate passages at 300 tokens
     collection = os.path.join(collection)
@@ -75,13 +85,23 @@ def indexer(checkpointp, index_name, collection):
     index_name = f'{index_name}.{nbits}bits'
 
 
-    with Run().context(RunConfig(nranks=1, experiment='notebook')):  # nranks specifies the number of GPUs to use.
+    with Run().context(RunConfig(nranks=1, experiment= experiment)):  # nranks specifies the number of GPUs to use.
         config = ColBERTConfig(doc_maxlen=doc_maxlen, nbits=nbits)
 
         indexer = Indexer(checkpoint=checkpoint, config=config)
         indexer.index(name=index_name, collection=collection, overwrite=True)
+    
+    # delete exist index
+    if os.path.exists(outputpath + "/" + index_name):
+        os.system("rm -rf " + outputpath + "/" + index_name)
+    # move the index to the output path
+    os.system("mv " + index_folder + index_name + " " + outputpath)
+    # print index path
+    print("Index path: " + outputpath + "/" + index_name)
+    # remove experiment folder
+    os.system("rm -rf experiments/")
 
 if __name__ == "__main__":
-    inputfile, outputfile = getvalue(sys.argv[1:])
-    jsontotsv(inputfile)
-    indexer(checkpointp, index_name, collection)
+    inputfile, outputpath = getvalue(sys.argv[1:])
+    collection = jsontotsv(inputfile)
+    indexer(checkpointp, index_name, collection, outputpath)
